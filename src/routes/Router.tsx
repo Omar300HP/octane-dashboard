07209b71b2ace9038b45660d5125c7d82cd43ef2
OmarEssam360 <omar.essam@atomica.ai>
2025-01-04 13:38:17 +0200

    refactor: routing to use centralized path definitions and add 404 handling

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Orders } from "./orders";
 import { Users } from "./users";
 import { Redirect } from "@/components/Redirect";
+import { routes } from "./paths";
 
+// TODO: Create the 404 page
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Redirect to="/orders" />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/users" element={<Users />} />
+        <Route
+          path={routes.home``}
+          element={<Redirect to={routes.orders``} />}
+        />
+        <Route path={routes.orders``} element={<Orders />} />
+        <Route path={routes.users``} element={<Users />} />
+        <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/paths.ts
@@ -0,0 +1,11 @@
+const routes = {
+  notFound: (_: TemplateStringsArray) => "/404",
+  home: (_: TemplateStringsArray) => "/",
+  orders: (_: TemplateStringsArray) => "/orders",
+  users: (_: TemplateStringsArray) => "/users",
+  userPage: (_: TemplateStringsArray, userId = ":userId") => `users/${userId}`,
+  orderPage: (_: TemplateStringsArray, orderId = ":orderId") =>
+    `orders/${orderId}`,
+} as const;
+
+export { routes };
